608644a68e543c4aa749dc1e42858ae0c639a7cd
UGF3ZWwgTW9za2Fsw7xr TW9za2Fsw7xr <kshantel@ga-t3.msk.21-school.ru>
2020-10-19 21:38:36 +0300

    fgbfgb

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "minishell.h"
+char	**g_env;
+
+int		main(int ac, char **av, char **env)
+{
+	int	i;
+
+	if (!(g_env = env))
+	return (0);
+}
---
+++ minishell.h
@@ -0,0 +1,19 @@
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <sys/stat.h>
+# include <sys/wait.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+
+
+
+
+#include <stdio.h>
+
+
+
+
+int		main(int ac, char **av, char **env);
+
+#endif
