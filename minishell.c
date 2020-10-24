96caf45aa8368c2ba4a16009ae1ffb250a421137
Dar'q Veresowa <tbeedril@oa-n5.msk.21-school.ru>
2020-10-24 14:13:33 +0300

    pizza update

--- minishell.c
+++ minishell.c
@@ -12,42 +12,116 @@ int	ft_strcmp(char *s1, char *s2)
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
 
+size_t ft_strlen2(char **full)
+{
+	char **s;
+	size_t len;
+
+	s = full;
+	len = 0;
+	while (*s != NULL)
+	{
+		s++;
+		len++;
+	}
+	return (len);
+}
+
 void	ft_do_cd(char **full)
 {
 	chdir(full[1]);
 }
 
+void	ft_printf_env(void)
+{
+	t_list *tmp;
+
+	tmp = g_env;
+	while (tmp)
+	{
+		printf("%s=%s\n", tmp->name, tmp->content);
+		tmp = tmp->next;
+	}
+}
+
+void	ft_unset(char **full)
+{
+	int i;
+	t_list *tmp, *srch;
+
+	i = 1;
+	while (full[i])
+	{
+		if (!(ft_strcmp(full[i], "_")))
+			i++;
+		else
+		{
+			tmp = g_env;
+			while (tmp && ft_strcmp(tmp->next->name, full[i]) != 0)
+				tmp = tmp->next;
+			if (tmp)
+			{
+				srch = tmp->next;
+				tmp->next = srch->next;
+				free(srch);
+			}
+			i++;
+		}
+	}
+}
+
+void	ft_export(char **full)
+{
+	char *p_e;
+	t_list *tmp;
+	int i;
+
+	i = 1;
+	while (full[i])
+	{
+		p_e = NULL;
+		tmp = g_env;
+		if (!(p_e = ft_strchr(full[i], '=')))
+			p_e = "";
+		else
+		{
+			*p_e = '\0';
+			p_e++;
+		}
+		while (tmp && ft_strcmp(tmp->name, full[i]) != 0)
+			tmp = tmp->next;
+		if (!tmp)
+			ft_lstadd_prev_back(&g_env, ft_lstnew(p_e, full[i]));
+		else
+		{
+			tmp->content = p_e;
+		}
+		i++;
+	}
+}
+
 void	ft_our_commands(char **full)
 {
-	int i = 1;
-	char *p_e = NULL;
-
 	if (!(ft_strcmp(full[0], "cd")))
-		chdir(full[1]);
+	{
+		if (ft_strlen2(full) > 2)
+			printf("cd: too many arguments\n");
+		else if (chdir(full[1]) == -1)
+			printf("cd: no such file or directory: %s\n", full[1]);
+	}
 	else if (!(ft_strcmp(full[0], "pwd")))
 	{
 		write(1, getcwd(NULL, 0), ft_strlen(getcwd(NULL, 0)));
 		write(1, "\n", 1);
 	}
+	else if(!(ft_strcmp(full[0], "env")))
+		ft_printf_env();
 	else if (!(ft_strcmp(full[0], "export")))
-	{
-		while (full[i])
-		{
-			p_e = ft_strchr(full[i], '=');
-			*p_e = '\0';
-			ft_lstadd_prev_back(&g_env, ft_lstnew(++p_e, full[i]));
-			i++;
-		}
-	}
+		ft_export(full);
+	else if (!(ft_strcmp(full[0], "unset")))
+		ft_unset(full);
 	else
-	{
-			t_list *tmp = g_env;
-	while (tmp)
-	{
-		printf("%s=%s\n", (char*)(tmp->name), (char *)(tmp->content));
-		tmp = tmp->next;
-	}
-	}
+		printf("minishell: command not found\n");
 }
 
 void	ft_exec(char **full)
